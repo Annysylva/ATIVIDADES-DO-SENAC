7cafd28d89e3456b0939ad60b0e917ff93ad935c
Anny Sylva <annybarbosa978@gmail.com>
2024-06-06 14:12:31 -0300

    Add files via upload

---
+++ 27maio.py/funções.py
@@ -0,0 +1,20 @@
+# criação de funções sem retorno
+
+def anny_sylva():
+    print("****************")
+    print("*  anny sylva  *")
+    print("****************")
+
+def python(inicio,fim):
+    for a in range (inicio,fim+1):
+        print(a,end=" ")
+
+def livro(texto):
+    print(texto)
+
+anny_sylva()
+print()
+livro("capitulo 7")
+print()
+python(7,10)
+            
